8c92e007f40db256a6fdd09afef205a68f22c056
田小椿 <569535228@qq.com>
2018-09-17 11:43:45 +0800

    //项目初始化

---
+++ GitTest/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  GitTest
+//
+//  Created by 田小椿 on 2018/9/17.
+//  Copyright © 2018年 xiangshengjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ GitTest/FirstViewController.m
@@ -0,0 +1,38 @@
+//
+//  FirstViewController.m
+//  GitTest
+//
+//  Created by 田小椿 on 2018/9/17.
+//  Copyright © 2018年 xiangshengjun. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"hello world");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
