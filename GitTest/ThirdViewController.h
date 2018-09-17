e4ce7265b30d54fc064af47ccea4151ac600688a
田小椿 <569535228@qq.com>
2018-09-17 12:08:19 +0800

    //添加第三个页面

---
+++ GitTest/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  GitTest
+//
+//  Created by 田小椿 on 2018/9/17.
+//  Copyright © 2018年 xiangshengjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
---
+++ GitTest/ThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  ThirdViewController.m
+//  GitTest
+//
+//  Created by 田小椿 on 2018/9/17.
+//  Copyright © 2018年 xiangshengjun. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"你好吗");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
